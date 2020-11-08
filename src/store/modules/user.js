d316be73b89ca3ffca233c851944ff76ae8d5b6a
tristan <tanshilinmail@gmail.com>
2020-11-08 15:42:35 +0800

    注销

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -74,21 +74,14 @@ const actions = {
 
   // user logout
   logout({commit, state, dispatch}) {
+    console.log("logout")
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '')
-        commit('SET_ROLES', [])
-        removeToken()
-        resetRouter()
-
-        // reset visited views and cached views
-        // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
-        dispatch('tagsView/delAllViews', null, {root: true})
-
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      removeToken()
+      resetRouter()
+      dispatch('tagsView/delAllViews', null, {root: true})
+      resolve()
     })
   },
 
