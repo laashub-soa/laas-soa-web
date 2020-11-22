f1fa55e7c83b1507d982e536317fc0734aebde3b
tristan-tsl <tanshilinmail@gmail.com>
2020-11-22 21:36:13 +0800

    数据树 	剔除表格中类型为数组的列、标志为关联数据模型的列

--- src/views/native/operate/data_data/DataData.vue
+++ src/views/native/operate/data_data/DataData.vue
@@ -24,9 +24,10 @@
       </i-form>
     </div>
 
-    <split>
+    <!--数据区域-->
+    <split v-if="is_show_data_area">
       <div slot="left"> <!--数据树-->
-
+        数据树
       </div>
       <div slot="right"> <!--数据-->
         <div>
@@ -43,9 +44,12 @@
             </div>
           </div>
         </div>
-
       </div>
     </split>
+    <div v-if="!is_show_data_area"> <!--数据树-->
+      数据树
+    </div>
+
     <div>
     </div>
   </div>
@@ -79,6 +83,7 @@
     },
     data() {
       return {
+        is_show_data_area: true,
         column_keys: [],
         columns: [],
         default_values: {},
@@ -103,22 +108,11 @@
           expand_status: false,
           data: {},
           template: [],
-        },
-        associate: {
-          trigger: {
-            "insert": [],
-            "update": [],
-            "delete": [],
-          },
-        },
-        data_event_2_logic: {
-          cur_choose: {
-            value: [],
-            logic_id: '',
-            func_name: '',
-          },
-          data: [],
-        },
+        }
+        , data_tree: {
+          keys: [],
+          values: [],
+        }
       }
     },
     methods: {
@@ -140,17 +134,30 @@
             width: 50
           });
           // basic column
-          for (const item of data_struct_list) {
-            const code = item["code"];
-            const meaning = item["meaning"];
-            this._data.default_values[code] = item["default_value"];
+          for (const data_struct of data_struct_list) {
+            // 剔除 标记为关联模型的列
+            if (data_struct["data_type"] != null) {
+              continue;
+            }
+            // 剔除data_type不为string的列
+            if (data_struct["data_type"] != "string") {
+              continue;
+            }
+            const code = data_struct["code"];
+            const meaning = data_struct["meaning"];
+            this._data.default_values[code] = data_struct["default_value"];
             this._data.column_keys.push(code);
             this._data.columns.push(component_table.editable_table_common_column(this, meaning, code, column_width));
             this._data.search.template.push({"label": meaning, "prop": code, "v_model": ""});
           }
-          // operation column
-          if (!this.is_open_data) {
-            this._data.columns.push(component_table.editable_table_common_operation_column(this));
+          if (this._data.columns.length > 1) {
+            // operation column
+            if (!this.is_open_data) {
+              this._data.columns.push(component_table.editable_table_common_operation_column(this));
+            }
+          } else {
+            // 隐藏 数据区域
+            this._data.is_show_data_area = false;
           }
           this.$Message.success('query data data columns success');
         } catch (e) {
@@ -197,10 +204,6 @@
           this._data.page.total = resp_data['page_total'];
           this._data.data = resp_data['data'];
           this.$Message.success('query data_struct success');
-          // await this.init_data_logic_trigger_status();
-          // status column
-          // if (this._data.columns.length < this._data.column_keys.length + 2)
-          //     this._data.columns.push(component_table.table_column_operation_status(this));
         } catch (e) {
           console.log(e);
           this.$Message.error(e.response.data);
@@ -208,31 +211,6 @@
           this._data.loading = false;
         }
       },
-      // async init_associate() {
-      //   // trigger
-      //   const designer_data_logic_trigger_list = await designer_data_logic_trigger.select_({'data_id': this.directory_id});
-      //   for (const item of designer_data_logic_trigger_list) {
-      //     this._data.associate.trigger[item["type"]].push(item["logic_id"] + ":" + item["func_name"]);
-      //   }
-      // },
-      // async init_data_logic_trigger_status() {
-      //   if (!this._data.data || this._data.data.length < 1) return;
-      //   const data_id = this.directory_id;
-      //   let data_data_data_id_list_str = "";
-      //   for (const item of this._data.data) {
-      //     const data_data_id = item["id"];
-      //     data_data_data_id_list_str += "'" + data_id + "_" + data_data_id + "', ";
-      //   }
-      //   data_data_data_id_list_str = data_data_data_id_list_str.substring(0, data_data_data_id_list_str.length - 2);
-      //   try {
-      //     let net_request_result = await designer_data_logic_trigger.select_batch_status({"data_data_data_id_list_str": data_data_data_id_list_str});
-      //     this._data.data_status = net_request_result;
-      //     this.$Message.success('query data logic trigger success');
-      //   } catch (e) {
-      //     console.log(e);
-      //     this.$Message.error(e.response.data);
-      //   }
-      // },
       init_insert_() {
         component_table.init_insert_(this);
       },
@@ -265,16 +243,17 @@
           // if (!await this.associate_data_logic_event(component, data_data, 'delete')) return;
           await designer_data_data.delete_(data_data);
           component.$Message.success('delete data data success');
-          await component.trigger_engine(component, 'delete', data_data['id']);
           await component.init_table();
         } catch (e) {
           console.log(e.response.data);
           component.$Message.error(e.response.data);
         }
-      },
+      }
+      , async init_data_tree() { // 初始化数据树
+        // TODO
+      }
     },
     async created() {
-      // await this.init_associate();
       await this.init_table_column();
       await this.init_table();
     }
