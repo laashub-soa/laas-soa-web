55445a420382b82b7c0032946aa7d8dac784ff83
tristan-tsl <tanshilinmail@gmail.com>
2020-11-22 15:21:54 +0800

    Project.vue

--- src/views/develop/Project.vue
+++ src/views/develop/Project.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <DataBoardData
+      :directory_id="directory_id"
+      :directory_name="directory_name" :split_value="split"></DataBoardData>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Project" // 建立项目
+  import DataBoardData from "@/views/native/operate/DataBoardData";
+
+  export default {
+    name: "Project",  // 项目
+    components: {
+      DataBoardData,
+    },
+    data() {
+      return {
+        directory_id: 24,
+        directory_name: '',
+        split: 0.2
+      }
     }
+  }
 </script>
-
-<style scoped>
-
-</style>
