19f7582550ffebef8137ed8f0fee7e9d43802332
tristan-tsl <tanshilinmail@gmail.com>
2020-11-23 18:02:17 +0800

    调整思路 CMDB仍然作为配置数据、基础数据维护 业务层暂时直接写, 不从CMDB带出

--- src/views/develop/project_config_file/UpdateProjectConfigFile.vue
+++ src/views/develop/project_config_file/UpdateProjectConfigFile.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <OperateDataData :data_data_pre_constraint="data_data_pre_constraint"></OperateDataData>
   </div>
 </template>
 
 <script>
-  import OperateDataData from '@/views/native/operate/data_data/OperateDataData.vue'
 
   export default {
     name: "UpdateProjectConfigFile" // 修改项目配置文件
-    , components: {
-      OperateDataData
-    }
+    , components: {}
     , data() {
-      return {
-        data_data_pre_constraint: [{"24": [{"25": ["26"]}]}]
-      }
+      return {}
     }
   }
 </script>
